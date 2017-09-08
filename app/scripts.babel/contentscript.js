d7d2284b003d8ac6153e7d779521aeaeb251e6d5
rachitGulati <rachitgulati26@gmail.com>
2017-09-08 08:04:26 +0530

    Fixed for all google domains

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -4,15 +4,25 @@ function injectJs(link) {
   scr.src = link;
   (document.head || document.body || document.documentElement).appendChild(scr);
 }
+function isNotAllowed() {
+  if (window.location && window.location.href.match(/google\.com/)) {
+    console.log(
+      "%cOops, JSconsole doesn't work on this website",
+      "color:#e5853d"
+    );
+    return false;
+  }
+  return true;
+}
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   var options = {};
   //Call from backgroun.js
-  if (request.action == "LOAD") {
+  if (request.action == "LOAD" && isNotAllowed()) {
     injectJs(chrome.extension.getURL("scripts/main.js"));
   }
 });
 chrome.storage.local.get("status", function(Obj) {
-  if (Obj.status !== "disabled") {
+  if (Obj.status !== "disabled" && isNotAllowed()) {
     injectJs(chrome.extension.getURL("scripts/main.js"));
   }
 });
